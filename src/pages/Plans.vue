e8a979141d22bdd835fba63db6be5329dc32fb0b
Alexander Currimjee <alexandercurrimjee@Alexanders-Air.lan>
2020-08-31 18:39:58 +0100

    plans center

--- src/pages/Plans.vue
+++ src/pages/Plans.vue
@@ -1,6 +1,6 @@
 <template>
   <OneSection>
-    <h1 data-aos="fade-up">{{Data.header.title}}</h1>
+    <h1 data-aos="fade-up" class="text-center">{{Data.header.title}}</h1>
     <p data-aos="fade-up" data-aos-delay="100">{{Data.header.subtext}}</p>
   </OneSection>
 </template>
