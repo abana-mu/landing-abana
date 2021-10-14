de654e2f7ac4e3608cd2bad02779ee02ab47e6b5
alexcurrimjee <alex.currimjee@gmail.com>
2021-10-14 20:00:52 +0100

    new pages

--- src/pages/PromoBuyer.vue
+++ src/pages/PromoBuyer.vue
@@ -2,11 +2,17 @@
   <Layout class="index-layout">
     <div id="parallax-over" class="relative z-1 bg-white">
       <div id="banner" class="relative w-full flex flex-col md:flex-row justify-center bg-white">
-        <div class="contain lg:h-full w-full flex flex-col justify-between z-10 ">
+        <div class="contain relative lg:h-full w-full flex flex-col justify-between z-10 ">
+          <div class="absolute opacity-50 right-0 top-0 w-full">
+            <Lottie :options="animationData" />
+          </div>
           <div class="w-full pt-24 pb-20 h-full text-center md:text-left flex flex-col justify-center items-center md:items-start">
-            <div class="marketing text-4xl lg:text-5xl lg:leading-normal font-light mb-6 max-w-5xl mx-auto text-title text-center mb-20" data-aos="fade-up" data-aos-duration="800">
-              Connect with <b>apparel manufacturers, fabric suppliers and service provicers</b> from Africa, Madagascar and Mauritius.
-            </div>
+            <vue-simple-markdown
+              :source="Data.header.title"
+              class="marketing text-4xl lg:text-5xl lg:leading-normal font-light mb-6 max-w-5xl mx-auto text-title text-center mb-20"
+              data-aos="fade-up"
+              data-aos-duration="800"
+            ></vue-simple-markdown>
 
             <div class="contain text-center" data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">
               <g-link to="/registration/" class="btn-xl btn-primary">Register as a free buyer</g-link>
@@ -15,36 +21,36 @@
         </div>
       </div>
 
-      <div class="relative overflow-hidden text-center text-gray-50 m-0">
+      <div class="relative overflow-hidden text-center text-gray-50 m-0 z-10">
         <svg class="w-full" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1366 64" preserveAspectRatio="none">
           <path fill="currentColor" d="M1366,64H0V.0337c205,0,709,55.6648,1366,55.6648Z"></path>
         </svg>
       </div>
 
-      <div class="bg-gray-50">
+      <div class="bg-gray-50 z-10 relative">
         <div class="contain py-24">
           <div>
             <div class="text-primary tracking-wide font-medium text-lg mb-4">Who are we</div>
             <div class="text-2xl font-medium mb-10 max-w-4xl">
-              The first textile & apparel sourcing platform that connects worldwide buyers, manufacturers and suppliers of Africa, Madagascar and Mauritius.
+              {{ Data.who.title }}
             </div>
           </div>
           <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-5 mb-10">
             <div class="p-10 bg-white shadow-xl rounded-xl">
               <g-image class="mx-auto mb-4 h-16 w-16" src="~/assets/images/database.svg" />
-              <div class="text-lg">Search our database to find detailed & relevant seller profiles</div>
+              <div class="text-lg">{{ Data.who.box1 }}</div>
             </div>
             <div class="p-10 bg-white shadow-xl rounded-xl">
               <g-image class="mx-auto mb-4 h-16 w-16" src="~/assets/images/direct.svg" />
-              <div class="text-lg">Deal directly with your suppliers, no commission fee is charged by ABANA</div>
+              <div class="text-lg">{{ Data.who.box2 }}</div>
             </div>
             <div class="p-10 bg-white shadow-xl rounded-xl">
               <g-image class="mx-auto mb-4 h-16 w-16" src="~/assets/images/opportunities.svg" />
-              <div class="text-lg">Find product and sourcing opportunities on our marketplace</div>
+              <div class="text-lg">{{ Data.who.box3 }}</div>
             </div>
             <div class="p-10 bg-white shadow-xl rounded-xl">
               <g-image class="mx-auto mb-4 h-16 w-16" src="~/assets/images/grow.svg" />
-              <div class="text-lg">Use our experience, network, and on-the-ground offline support to grow in the region</div>
+              <div class="text-lg">{{ Data.who.box4 }}</div>
             </div>
           </div>
           <div class="contain text-center">
@@ -60,43 +66,42 @@
       </div>
 
       <div class="contain-tight py-28">
-        <div class="text-4xl lg:text-5xl mb-4 font-bold text-title text-center mb-24">Our value-added services:</div>
+        <div class="text-4xl lg:text-5xl mb-4 font-bold text-title text-center mb-24">{{ Data.vad.title }}</div>
 
         <div class="space-y-20">
           <div class="grid md:grid-cols-2 items-center gap-10">
             <div class="flex flex-col">
-              <div class="text-2xl font-medium text-primary mb-4">Personalised advice</div>
+              <div class="text-2xl font-medium text-primary mb-4">{{ Data.vad.title1 }}</div>
               <div class="text-lg">
-                We can help you on woven, knit, denim and jersey apparel production from basic to premium. Tell us what you are looking for and we connect you with vetted suppliers based on your
-                specific business needs (product, price, MOQ, lead times).
+                {{ Data.vad.text1 }}
               </div>
             </div>
             <div class="">
-              <g-image src="/uploads/country-rep.jpg" quality="100" width="750" class="rounded-md w-full shadow-xl" />
+              <g-image :src="Data.vad.image1" quality="100" width="750" class="rounded-md w-full shadow-xl" />
             </div>
           </div>
 
           <div class="grid md:grid-cols-2 items-center gap-10">
             <div class="flex flex-col">
-              <div class="text-2xl font-medium text-primary mb-4">Agency</div>
+              <div class="text-2xl font-medium text-primary mb-4">{{ Data.vad.title2 }}</div>
               <div class="text-lg">
-                Save time and build your supply chain in Africa with our on-the-ground support. Pick and choose from our range of services tailored to your business needs.
+                {{ Data.vad.text2 }}
               </div>
             </div>
             <div class="">
-              <g-image src="/uploads/jeans-2.jpg" quality="100" width="750" class="rounded-md w-full shadow-xl" />
+              <g-image :src="Data.vad.image2" quality="100" width="750" class="rounded-md w-full shadow-xl" />
             </div>
           </div>
 
           <div class="grid md:grid-cols-2 items-center gap-10">
             <div class="flex flex-col">
-              <div class="text-2xl font-medium text-primary mb-4">Virtual Factory</div>
+              <div class="text-2xl font-medium text-primary mb-4">{{ Data.vad.title3 }}</div>
               <div class="text-lg">
-                Use us as your one-stop shop supplier solution to access our trusted specialised factories producing woven, denim, jersey and knitwear.
+                {{ Data.vad.text3 }}
               </div>
             </div>
             <div class="">
-              <g-image src="/uploads/knit2.jpg" quality="100" width="750" class="rounded-md w-full shadow-xl" />
+              <g-image :src="Data.vad.image3" quality="100" width="750" class="rounded-md w-full shadow-xl" />
             </div>
           </div>
         </div>
@@ -104,12 +109,8 @@
 
       <div class="bg-gray-50 border-t border-b">
         <div class="contain-tight py-28">
-          <div class="text-4xl lg:text-5xl mb-4 font-bold text-title text-center mb-8">A trusted partner at your side</div>
-          <div class="text-2xl text-center">
-            More than <span class="text-primary font-medium">65 years</span> of combined textile sector experience as Makers & Buyers, working with industry-leading factories.
-            <span class="text-primary font-medium">ABANA is a one-stop shop</span>
-            inclusively connecting all modern industry players throughout the full supply chain.
-          </div>
+          <div class="text-4xl lg:text-5xl mb-4 font-bold text-title text-center mb-8">{{ Data.tp.title }}</div>
+          <vue-simple-markdown :source="Data.tp.text" class="marketing text-2xl text-center" data-aos="fade-up" data-aos-duration="800"></vue-simple-markdown>
         </div>
       </div>
     </div>
@@ -146,11 +147,13 @@
         </div>
       </div>
     </div>
+
     <div class="bg-gray-50 border-t border-b">
       <div class="contain my-12">
-        <IndexTrusted :data="Data.trusted" class="w-full max-w-screen-lg mx-auto" />
+        <IndexTrusted :data="TrustedData.trusted" class="w-full max-w-screen-lg mx-auto" />
       </div>
     </div>
+
     <div class="contain text-center my-20">
       <g-link to="/registration/" class="btn-2xl btn-primary">Register as a free buyer</g-link>
     </div>
@@ -178,26 +181,10 @@ query{
 </page-query>
 
 <script>
-import IndexBanner from '~/components/IndexBanner';
-import Data from '~/_settings/landing.json';
-import Flow from '~/components/icons/Flow';
-import ITransf from '@/components/icons/ITransf';
-import IExpect from '@/components/icons/IExpect';
-import IGuar from '@/components/icons/IGuar';
-import ITax from '@/components/icons/ITax';
-import ITarget from '@/components/icons/ITarget';
+import Data from '~/_settings/promo-buyer.json';
+import TrustedData from '~/_settings/landing.json';
 import Lottie from 'vue-lottie';
-import supportAnimation from '@/assets/animation/support.json';
-import networkAnimation from '@/assets/animation/network.json';
-
-import INew from '@/components/icons/INew';
-import IChina from '@/components/icons/IChina';
-import IEnv from '@/components/icons/IEnv';
-import ICheck from '@/components/icons/ICheck';
-import ICal from '@/components/icons/ICal';
-import IndexFlowGraph from '~/components/IndexFlowGraph';
-import IndexDropGraph from '~/components/IndexDropGraph';
-import IndexRepsGraph from '~/components/IndexRepsGraph';
+import Animation from '@/assets/animation/landing.json';
 import IndexTrusted from '~/components/IndexTrusted';
 
 export default {
@@ -212,22 +199,13 @@ export default {
   },
   components: {
     Lottie,
-    IndexBanner,
-    box1: IChina,
-    box2: ICheck,
-    box3: IEnv,
-    box4: ICal,
-    IndexFlowGraph,
-    IndexDropGraph,
-    IndexRepsGraph,
     IndexTrusted,
-    Flow,
   },
   data() {
     return {
       Data,
-      networkAnimationData: { animationData: networkAnimation },
-      supportAnimationData: { animationData: supportAnimation },
+      TrustedData,
+      animationData: { animationData: Animation },
     };
   },
   mounted() {
@@ -264,29 +242,6 @@ export default {
   destroyed() {
     window.removeEventListener('scroll', this.handleScroll);
   },
-  methods: {
-    isBlockScrolled(el) {
-      var block = el.getBoundingClientRect();
-      return block.top <= window.innerHeight * 0.5;
-    },
-    stickyElement() {
-      var blocks = document.querySelectorAll('.block-item');
-      blocks.forEach((block, index) => {
-        let imageBlock = document.getElementById(block.id + '-image');
-
-        if (index % 10 != 0) {
-          if (this.isBlockScrolled(block)) {
-            imageBlock.classList.remove('opacity-0');
-          } else {
-            imageBlock.classList.add('opacity-0');
-          }
-        }
-      });
-    },
-    handleScroll() {
-      this.stickyElement();
-    },
-  },
 };
 </script>
 
@@ -298,31 +253,8 @@ footer,
   }
 }
 
-#banner-image {
-  width: 125%;
-}
-
-#image-container {
-  height: calc(100vh - 11.875rem);
-  top: 90px;
-  padding: 3.75rem 0;
-}
-
 .home-links a {
   margin-right: 1rem;
-}
-
-#boxTitle {
-  text-align: center;
-  margin-bottom: 1rem;
-}
-
-#boxSub {
-  margin: auto auto;
-  color: $subtitle-dark;
-  margin-bottom: 2rem;
-  font-weight: 500;
-  text-align: center;
 }
 
 .box-row {
--- src/pages/PromoSeller.vue
+++ src/pages/PromoSeller.vue
@@ -2,15 +2,20 @@
   <Layout class="index-layout">
     <div id="parallax-over" class="relative z-1 bg-white">
       <div id="banner" class="relative w-full flex flex-col md:flex-row justify-center bg-white">
-        <div class="contain lg:h-full w-full flex flex-col justify-between z-10 ">
-          <div class="w-full pt-24 pb-20 h-full text-center md:text-left flex flex-col justify-center items-center md:items-start">
-            <div class="marketing text-4xl lg:text-5xl lg:leading-normal font-medium mb-6 max-w-5xl mx-auto text-title text-center mb-10" data-aos="fade-up" data-aos-duration="800">
-              Connect to new <b>Buyers, Yarn, Fabric & Trims Suppliers, and Service Providers</b>
-            </div>
+        <div class="contain relative lg:h-full w-full flex flex-col justify-between z-10 ">
+          <div class="absolute opacity-30 right-0 top-0 w-full">
+            <Lottie :options="animationData" />
+          </div>
+          <div class="z-10 w-full pt-24 pb-20 h-full text-center md:text-left flex flex-col justify-center items-center md:items-start">
+            <vue-simple-markdown
+              :source="Data.header.title"
+              class="marketing text-4xl lg:text-5xl lg:leading-normal font-light mb-6 max-w-5xl mx-auto text-title text-center mb-10"
+              data-aos="fade-up"
+              data-aos-duration="800"
+            ></vue-simple-markdown>
 
             <div class="max-w-3xl mx-auto text-2xl mb-10 text-center">
-              ABANA is a one-stop shop inclusively connecting all industry players throughout the full supply chain. Join our network and be visible to international buyers looking for reliable
-              partners in Africa.
+              {{ Data.header.subtext }}
             </div>
 
             <div class="contain text-center">
@@ -20,35 +25,35 @@
         </div>
       </div>
 
-      <div class="relative overflow-hidden text-center text-gray-50 m-0">
+      <div class="relative overflow-hidden text-center text-gray-50 m-0 z-10">
         <svg class="w-full" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1366 64" preserveAspectRatio="none">
           <path fill="currentColor" d="M1366,64H0V.0337c205,0,709,55.6648,1366,55.6648Z"></path>
         </svg>
       </div>
 
-      <div class="bg-gray-50">
+      <div class="bg-gray-50 relative z-10">
         <div class="contain py-24">
-          <div class="text-3xl lg:text-4xl mb-4 font-bold text-title text-center mb-24">Fast and Easy!</div>
+          <div class="text-3xl lg:text-4xl mb-4 font-bold text-title text-center mb-24">{{ Data.steps.title }}</div>
           <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-5 mb-10">
             <div>
               <g-image class="mb-2 h-10 w-10" src="~/assets/images/account.svg" />
               <div class="text-lg tracking-wider font-medium text-primary">Step 1</div>
-              <div class="text-2xl">Create your account</div>
+              <div class="text-2xl">{{ Data.steps.box1 }}</div>
             </div>
             <div>
               <g-image class="mb-2 h-10 w-10" src="~/assets/images/product.svg" />
               <div class="text-lg tracking-wider font-medium text-primary">Step 2</div>
-              <div class="text-2xl">Showcase your Product</div>
+              <div class="text-2xl">{{ Data.steps.box2 }}</div>
             </div>
             <div>
               <g-image class="mb-2 h-10 w-10" src="~/assets/images/respond.svg" />
               <div class="text-lg tracking-wider font-medium text-primary">Step 3</div>
-              <div class="text-2xl">Respond to quotes</div>
+              <div class="text-2xl">{{ Data.steps.box3 }}</div>
             </div>
             <div>
               <g-image class="mb-2 h-10 w-10" src="~/assets/images/partnerships.svg" />
               <div class="text-lg tracking-wider font-medium text-primary">Step 4</div>
-              <div class="text-2xl">Build new partnerships</div>
+              <div class="text-2xl">{{ Data.steps.box4 }}</div>
             </div>
           </div>
         </div>
@@ -60,56 +65,51 @@
         </svg>
       </div>
 
+      <div class="">
+        <div class="contain-tight py-28">
+          <div class="text-4xl lg:text-5xl mb-4 font-bold text-title text-center mb-8">{{ Data.tp.title }}</div>
+          <vue-simple-markdown :source="Data.tp.text" class="marketing text-2xl text-center" data-aos="fade-up" data-aos-duration="800"></vue-simple-markdown>
+        </div>
+      </div>
+
       <div class="contain-tight py-28">
-        <div class="text-4xl lg:text-5xl mb-4 font-bold text-title text-center mb-24">Our value-added services:</div>
+        <div class="text-4xl lg:text-5xl mb-4 font-bold text-title text-center mb-24">{{ Data.vad.title }}</div>
 
         <div class="space-y-20">
           <div class="grid md:grid-cols-2 items-center gap-10">
             <div class="flex flex-col">
-              <div class="text-2xl font-medium text-primary mb-4">Personalised advice</div>
+              <div class="text-2xl font-medium text-primary mb-4">{{ Data.vad.title1 }}</div>
               <div class="text-lg">
-                We can help you on woven, knit, denim and jersey apparel production from basic to premium. Tell us what you are looking for and we connect you with vetted suppliers based on your
-                specific business needs (product, price, MOQ, lead times).
+                {{ Data.vad.text1 }}
               </div>
             </div>
             <div class="">
-              <g-image src="/uploads/country-rep.jpg" quality="100" width="750" class="rounded-md w-full shadow-xl" />
+              <g-image :src="Data.vad.image1" quality="100" width="750" class="rounded-md w-full shadow-xl" />
             </div>
           </div>
 
           <div class="grid md:grid-cols-2 items-center gap-10">
             <div class="flex flex-col">
-              <div class="text-2xl font-medium text-primary mb-4">Agency</div>
+              <div class="text-2xl font-medium text-primary mb-4">{{ Data.vad.title2 }}</div>
               <div class="text-lg">
-                Save time and build your supply chain in Africa with our on-the-ground support. Pick and choose from our range of services tailored to your business needs.
+                {{ Data.vad.text2 }}
               </div>
             </div>
             <div class="">
-              <g-image src="/uploads/jeans-2.jpg" quality="100" width="750" class="rounded-md w-full shadow-xl" />
+              <g-image :src="Data.vad.image2" quality="100" width="750" class="rounded-md w-full shadow-xl" />
             </div>
           </div>
 
           <div class="grid md:grid-cols-2 items-center gap-10">
             <div class="flex flex-col">
-              <div class="text-2xl font-medium text-primary mb-4">Virtual Factory</div>
+              <div class="text-2xl font-medium text-primary mb-4">{{ Data.vad.title3 }}</div>
               <div class="text-lg">
-                Use us as your one-stop shop supplier solution to access our trusted specialised factories producing woven, denim, jersey and knitwear.
+                {{ Data.vad.text3 }}
               </div>
             </div>
             <div class="">
-              <g-image src="/uploads/knit2.jpg" quality="100" width="750" class="rounded-md w-full shadow-xl" />
+              <g-image :src="Data.vad.image3" quality="100" width="750" class="rounded-md w-full shadow-xl" />
             </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="bg-gray-50 border-t border-b">
-        <div class="contain-tight py-28">
-          <div class="text-4xl lg:text-5xl mb-4 font-bold text-title text-center mb-8">A trusted partner at your side</div>
-          <div class="text-2xl text-center">
-            More than <span class="text-primary font-medium">65 years</span> of combined textile sector experience as Makers & Buyers, working with industry-leading factories.
-            <span class="text-primary font-medium">ABANA is a one-stop shop</span>
-            inclusively connecting all modern industry players throughout the full supply chain.
           </div>
         </div>
       </div>
@@ -147,11 +147,13 @@
         </div>
       </div>
     </div>
+
     <div class="bg-gray-50 border-t border-b">
       <div class="contain my-12">
-        <IndexTrusted :data="Data.trusted" class="w-full max-w-screen-lg mx-auto" />
+        <IndexTrusted :data="TrustedData.trusted" class="w-full max-w-screen-lg mx-auto" />
       </div>
     </div>
+
     <div class="contain text-center my-20">
       <g-link to="/registration/" class="btn-2xl btn-primary">Register as a free buyer</g-link>
     </div>
@@ -177,28 +179,11 @@ query{
   }
 }
 </page-query>
-
 <script>
-import IndexBanner from '~/components/IndexBanner';
-import Data from '~/_settings/landing.json';
-import Flow from '~/components/icons/Flow';
-import ITransf from '@/components/icons/ITransf';
-import IExpect from '@/components/icons/IExpect';
-import IGuar from '@/components/icons/IGuar';
-import ITax from '@/components/icons/ITax';
-import ITarget from '@/components/icons/ITarget';
+import Data from '~/_settings/promo-seller.json';
+import TrustedData from '~/_settings/landing.json';
 import Lottie from 'vue-lottie';
-import supportAnimation from '@/assets/animation/support.json';
-import networkAnimation from '@/assets/animation/network.json';
-
-import INew from '@/components/icons/INew';
-import IChina from '@/components/icons/IChina';
-import IEnv from '@/components/icons/IEnv';
-import ICheck from '@/components/icons/ICheck';
-import ICal from '@/components/icons/ICal';
-import IndexFlowGraph from '~/components/IndexFlowGraph';
-import IndexDropGraph from '~/components/IndexDropGraph';
-import IndexRepsGraph from '~/components/IndexRepsGraph';
+import Animation from '@/assets/animation/landing.json';
 import IndexTrusted from '~/components/IndexTrusted';
 
 export default {
@@ -213,22 +198,13 @@ export default {
   },
   components: {
     Lottie,
-    IndexBanner,
-    box1: IChina,
-    box2: ICheck,
-    box3: IEnv,
-    box4: ICal,
-    IndexFlowGraph,
-    IndexDropGraph,
-    IndexRepsGraph,
     IndexTrusted,
-    Flow,
   },
   data() {
     return {
       Data,
-      networkAnimationData: { animationData: networkAnimation },
-      supportAnimationData: { animationData: supportAnimation },
+      TrustedData,
+      animationData: { animationData: Animation },
     };
   },
   mounted() {
@@ -265,72 +241,13 @@ export default {
   destroyed() {
     window.removeEventListener('scroll', this.handleScroll);
   },
-  methods: {
-    isBlockScrolled(el) {
-      var block = el.getBoundingClientRect();
-      return block.top <= window.innerHeight * 0.5;
-    },
-    stickyElement() {
-      var blocks = document.querySelectorAll('.block-item');
-      blocks.forEach((block, index) => {
-        let imageBlock = document.getElementById(block.id + '-image');
-
-        if (index % 10 != 0) {
-          if (this.isBlockScrolled(block)) {
-            imageBlock.classList.remove('opacity-0');
-          } else {
-            imageBlock.classList.add('opacity-0');
-          }
-        }
-      });
-    },
-    handleScroll() {
-      this.stickyElement();
-    },
-  },
 };
 </script>
-
 <style lang="scss" scoped>
 footer,
 #parallax-under {
   @media (max-width: $break-md) {
     position: static !important;
-  }
-}
-
-#banner-image {
-  width: 125%;
-}
-
-#image-container {
-  height: calc(100vh - 11.875rem);
-  top: 90px;
-  padding: 3.75rem 0;
-}
-
-.home-links a {
-  margin-right: 1rem;
-}
-
-#boxTitle {
-  text-align: center;
-  margin-bottom: 1rem;
-}
-
-#boxSub {
-  margin: auto auto;
-  color: $subtitle-dark;
-  margin-bottom: 2rem;
-  font-weight: 500;
-  text-align: center;
-}
-
-.box-row {
-  display: flex;
-  flex-direction: column;
-  @media (min-width: $break-md) {
-    flex-direction: row;
   }
 }
 </style>
