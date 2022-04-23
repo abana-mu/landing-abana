51b869a828435a76a228ad6d6a33a8f7e3cd57ed
alexcurrimjee <alex.currimjee@gmail.com>
2022-04-23 17:23:55 +0100

    added youtube videos

--- src/pages/ConnectWithManufacturers.vue
+++ src/pages/ConnectWithManufacturers.vue
@@ -1,16 +1,25 @@
 <template>
   <Layout class="index-layout">
     <div v-if="modal">
-      <Modal :title="Data.popup.title" :description="Data.popup.text" :cta="Data.popup.cta" :warning="Data.popup.warning" :close="closeModal" :id="1" />
+      <Modal
+        :title="Data.popup.title"
+        :description="Data.popup.text"
+        :cta="Data.popup.cta"
+        :warning="Data.popup.warning"
+        :close="closeModal"
+        :id="1"
+      />
     </div>
 
     <div id="parallax-over" class="relative z-1 bg-white">
       <div id="banner" class="relative w-full flex flex-col md:flex-row justify-center bg-white">
-        <div class="contain relative lg:h-full w-full flex flex-col justify-between z-10 ">
+        <div class="contain relative lg:h-full w-full flex flex-col justify-between z-10">
           <div class="z-0 absolute opacity-50 right-0 top-0 w-full lottie-banner">
             <Lottie :options="animationData" />
           </div>
-          <div class="z-10 w-full pt-24 pb-20 h-full text-center md:text-left flex flex-col justify-center items-center md:items-start">
+          <div
+            class="z-10 w-full pt-24 pb-20 h-full text-center md:text-left flex flex-col justify-center items-center md:items-start"
+          >
             <vue-simple-markdown
               :source="Data.header.title"
               class="marketing text-4xl lg:text-5xl lg:leading-normal font-light mb-6 max-w-5xl mx-auto text-title text-center mb-20"
@@ -24,8 +33,14 @@
       </div>
 
       <div class="relative overflow-hidden text-center text-gray-50 m-0 z-10">
-        <svg class="w-full" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1366 64" preserveAspectRatio="none">
-          <path fill="currentColor" d="M1366,64H0V.0337c205,0,709,55.6648,1366,55.6648Z"></path>
+        <svg
+          class="w-full"
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1366 64"
+          preserveAspectRatio="none"
+        >
+          <path fill="currentColor" d="M1366,64H0V.0337c205,0,709,55.6648,1366,55.6648Z" />
         </svg>
       </div>
 
@@ -33,8 +48,18 @@
         <div class="contain py-24">
           <div>
             <div class="text-primary tracking-wide font-medium text-lg mb-4">Who are we</div>
-            <div class="text-2xl font-medium mb-10 max-w-4xl">
-              {{ Data.who.title }}
+            <div class="text-2xl font-medium mb-10 max-w-4xl">{{ Data.who.title }}</div>
+          </div>
+          <div class="max-w-3xl mx-auto mb-12">
+            <div class="video-container">
+              <iframe
+                class="video-iframe"
+                src="https://www.youtube.com/embed/YfTbM_y8dYQ"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+                modestbranding
+              ></iframe>
             </div>
           </div>
           <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-5 mb-10">
@@ -62,48 +87,65 @@
       </div>
 
       <div class="relative overflow-hidden text-center text-gray-50 m-0">
-        <svg class="w-full transform rotate-180" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1366 64" preserveAspectRatio="none">
-          <path fill="currentColor" d="M1366,64H0V.0337c205,0,709,55.6648,1366,55.6648Z"></path>
+        <svg
+          class="w-full transform rotate-180"
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1366 64"
+          preserveAspectRatio="none"
+        >
+          <path fill="currentColor" d="M1366,64H0V.0337c205,0,709,55.6648,1366,55.6648Z" />
         </svg>
       </div>
 
       <div class="contain-tight py-28">
-        <div class="text-4xl lg:text-5xl mb-4 font-bold text-title text-center mb-24">{{ Data.vad.title }}</div>
+        <div
+          class="text-4xl lg:text-5xl mb-4 font-bold text-title text-center mb-24"
+        >{{ Data.vad.title }}</div>
 
         <div class="space-y-20">
           <div class="grid md:grid-cols-2 items-center gap-10">
             <div class="flex flex-col">
               <div class="text-2xl font-medium text-primary mb-4">{{ Data.vad.title1 }}</div>
-              <div class="text-lg">
-                {{ Data.vad.text1 }}
-              </div>
+              <div class="text-lg">{{ Data.vad.text1 }}</div>
             </div>
-            <div class="">
-              <g-image :src="Data.vad.image1" quality="100" width="750" class="rounded-md w-full shadow-xl" />
+            <div class>
+              <g-image
+                :src="Data.vad.image1"
+                quality="100"
+                width="750"
+                class="rounded-md w-full shadow-xl"
+              />
             </div>
           </div>
 
           <div class="grid md:grid-cols-2 items-center gap-10">
             <div class="flex flex-col">
               <div class="text-2xl font-medium text-primary mb-4">{{ Data.vad.title2 }}</div>
-              <div class="text-lg">
-                {{ Data.vad.text2 }}
-              </div>
+              <div class="text-lg">{{ Data.vad.text2 }}</div>
             </div>
-            <div class="">
-              <g-image :src="Data.vad.image2" quality="100" width="750" class="rounded-md w-full shadow-xl" />
+            <div class>
+              <g-image
+                :src="Data.vad.image2"
+                quality="100"
+                width="750"
+                class="rounded-md w-full shadow-xl"
+              />
             </div>
           </div>
 
           <div class="grid md:grid-cols-2 items-center gap-10">
             <div class="flex flex-col">
               <div class="text-2xl font-medium text-primary mb-4">{{ Data.vad.title3 }}</div>
-              <div class="text-lg">
-                {{ Data.vad.text3 }}
-              </div>
+              <div class="text-lg">{{ Data.vad.text3 }}</div>
             </div>
-            <div class="">
-              <g-image :src="Data.vad.image3" quality="100" width="750" class="rounded-md w-full shadow-xl" />
+            <div class>
+              <g-image
+                :src="Data.vad.image3"
+                quality="100"
+                width="750"
+                class="rounded-md w-full shadow-xl"
+              />
             </div>
           </div>
         </div>
@@ -111,19 +153,32 @@
 
       <div class="bg-gray-50 border-t border-b">
         <div class="contain-tight py-28">
-          <div class="text-4xl lg:text-5xl mb-4 font-bold text-title text-center mb-8">{{ Data.tp.title }}</div>
-          <vue-simple-markdown :source="Data.tp.text" class="marketing text-2xl text-center" data-aos="fade-up" data-aos-duration="800"></vue-simple-markdown>
+          <div
+            class="text-4xl lg:text-5xl mb-4 font-bold text-title text-center mb-8"
+          >{{ Data.tp.title }}</div>
+          <vue-simple-markdown
+            :source="Data.tp.text"
+            class="marketing text-2xl text-center"
+            data-aos="fade-up"
+            data-aos-duration="800"
+          ></vue-simple-markdown>
         </div>
       </div>
     </div>
     <div id="spacer" class="hidden md:flex"></div>
     <div id="parallax-under" class="overflow-hidden bg-primary md:h-full top-0 z-0 w-full">
       <div class="contain pt-28">
-        <h2 class="text-2xl lg:text-5xl font-medium mb-16 text-center text-primary-100">What industry leading voices are saying:</h2>
+        <h2
+          class="text-2xl lg:text-5xl font-medium mb-16 text-center text-primary-100"
+        >What industry leading voices are saying:</h2>
 
         <div class="py-4 mb-12">
           <div class="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-4xl mx-auto">
-            <div class="flex flex-col justify-around bg-white rounded-lg shadow-xl border px-8 py-16" v-for="testi in $page.posts.edges" :key="testi.id">
+            <div
+              class="flex flex-col justify-around bg-white rounded-lg shadow-xl border px-8 py-16"
+              v-for="testi in $page.posts.edges"
+              :key="testi.id"
+            >
               <div class="w-full max-w-xs px-16 mb-8">
                 <g-image :src="testi.node.logo" class="w-full" />
               </div>
@@ -183,27 +238,28 @@ query{
 </page-query>
 
 <script>
-import Data from '~/_settings/promo-buyer.json';
-import TrustedData from '~/_settings/landing.json';
-import Lottie from 'vue-lottie';
-import Animation from '@/assets/animation/landing.json';
-import IndexTrusted from '~/components/IndexTrusted';
-import Modal from '~/components/Modal';
+import Data from "~/_settings/promo-buyer.json";
+import TrustedData from "~/_settings/landing.json";
+import Lottie from "vue-lottie";
+import Animation from "@/assets/animation/landing.json";
+import IndexTrusted from "~/components/IndexTrusted";
+import Modal from "~/components/Modal";
 
 export default {
   metaInfo: {
-    title: 'ABANA | African Textile & Apparel Marketplace',
+    title: "ABANA | African Textile & Apparel Marketplace",
     meta: [
       {
-        name: 'description',
-        content: 'Welcome to the first B2B sourcing platform for Textile and Apparel Africa. We help Textile and Apparel buyers find an connect to suppliers.',
-      },
-    ],
+        name: "description",
+        content:
+          "Welcome to the first B2B sourcing platform for Textile and Apparel Africa. We help Textile and Apparel buyers find an connect to suppliers."
+      }
+    ]
   },
   components: {
     Lottie,
     IndexTrusted,
-    Modal,
+    Modal
   },
   data() {
     return {
@@ -211,38 +267,47 @@ export default {
       TrustedData,
       animationData: { animationData: Animation },
       modal: false,
-      opened: false,
+      opened: false
     };
   },
   mounted() {
     var width = window.innerWidth;
     var that = this;
-    var height = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+    var height = Math.max(
+      document.body.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.clientHeight,
+      document.documentElement.scrollHeight,
+      document.documentElement.offsetHeight
+    );
 
     if (width > 768) {
-      var footer = document.querySelector('footer');
-      var over = document.getElementById('parallax-over');
-      var spacer = document.getElementById('spacer');
-      var under = document.getElementById('parallax-under');
-      footer.classList.add('fixed');
-      under.classList.add('fixed');
-      under.classList.add('opacity-0');
-      spacer.style.height = under.getBoundingClientRect().height + footer.getBoundingClientRect().height + 'px';
-      window.addEventListener('scroll', function() {
+      var footer = document.querySelector("footer");
+      var over = document.getElementById("parallax-over");
+      var spacer = document.getElementById("spacer");
+      var under = document.getElementById("parallax-under");
+      footer.classList.add("fixed");
+      under.classList.add("fixed");
+      under.classList.add("opacity-0");
+      spacer.style.height =
+        under.getBoundingClientRect().height +
+        footer.getBoundingClientRect().height +
+        "px";
+      window.addEventListener("scroll", function() {
         if (window.scrollY > 40) {
-          under.classList.remove('opacity-0');
+          under.classList.remove("opacity-0");
         } else {
-          under.classList.add('opacity-0');
+          under.classList.add("opacity-0");
         }
         if (over.getBoundingClientRect().bottom > 1) {
-          spacer.classList.remove('fixed');
-          footer.classList.add('fixed');
-          under.classList.add('fixed');
+          spacer.classList.remove("fixed");
+          footer.classList.add("fixed");
+          under.classList.add("fixed");
         }
         if (over.getBoundingClientRect().bottom <= 1) {
-          spacer.classList.add('fixed');
-          footer.classList.remove('fixed');
-          under.classList.remove('fixed');
+          spacer.classList.add("fixed");
+          footer.classList.remove("fixed");
+          under.classList.remove("fixed");
         }
 
         if (window.scrollY > height / 2 && !localStorage.opened) {
@@ -252,7 +317,7 @@ export default {
     }
   },
   destroyed() {
-    window.removeEventListener('scroll', this.handleScroll);
+    window.removeEventListener("scroll", this.handleScroll);
   },
   methods: {
     closeModal() {
@@ -261,8 +326,8 @@ export default {
     openModal() {
       this.modal = true;
       localStorage.opened = true;
-    },
-  },
+    }
+  }
 };
 </script>
 
@@ -272,6 +337,22 @@ footer,
   @media (max-width: $break-md) {
     position: static !important;
   }
+}
+
+.video-container {
+  position: relative;
+  padding-bottom: 56.25%;
+  padding-top: 30px;
+  height: 0;
+  overflow: hidden;
+}
+
+.video-iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 
 .lottie-banner {
